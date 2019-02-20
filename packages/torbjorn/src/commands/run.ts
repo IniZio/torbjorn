7c40cb5ca9393ccaf73d18309dcb23346eb0df38
inizio <newman.kcchow@gmail.com>
2019-02-20 22:05:40 +0800

    refactor(dispatch): merge dispatch command into run

--- packages/torbjorn/src/commands/run.ts
+++ packages/torbjorn/src/commands/run.ts
@@ -3,10 +3,14 @@ import CAC from 'cac/types/CAC'
 
 import Torbjorn from '../instance'
 
+const opt2Arr = val => val ? [].concat(val).reduce((acc, o) => [...acc, ...(typeof o === 'string' ? o.split(',') : o)], []) : val
+
 export function initRun(terminal: CAC, torbjorn: Torbjorn): void {
   terminal
     .command('run', 'Runs given config')
-    .action(async () => {
-      await torbjorn.run()
+    .option('--only <only>', 'Only include certain actions')
+    .option('--except <except>', 'Exclude certain actions')
+    .action(async ({only, except}) => {
+      await torbjorn.run({only: opt2Arr(only), except: opt2Arr(except)})
     })
 }
