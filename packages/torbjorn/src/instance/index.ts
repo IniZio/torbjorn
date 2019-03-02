4521ee7b241088091f29b92a495d9e60443adc69
inizio <newman.kcchow@gmail.com>
2019-03-02 13:55:14 +0800

    refactor(instance): cleaner instance api

--- packages/torbjorn/src/instance/index.ts
+++ packages/torbjorn/src/instance/index.ts
@@ -1,10 +1,11 @@
+import {flow} from 'lodash/fp'
+
 import BaseTorbjorn from './base'
 
 import {addTools} from './tool'
 import {addInclude} from './include'
 import {addConfig} from './config'
-import {addAction} from './action'
 
-class Torbjorn extends addAction(addConfig(addInclude(addTools(BaseTorbjorn)))) {}
+class Torbjorn extends flow(addConfig, addInclude, addTools)(BaseTorbjorn) {}
 
 export default Torbjorn
